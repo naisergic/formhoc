965c1dae4000e5f78ee8026c0faffed1167aceb0
naisergic <nsharma82@sapient.com>
2018-09-02 21:02:10 +0530

    removing node_modules

---
+++ form-hoc/README.md
@@ -0,0 +1,31 @@
+# form-hoc
+
+> it is a HOC for all of the form input type it will handle error and also maintain the state of every form input type
+
+[![NPM](https://img.shields.io/npm/v/form-hoc.svg)](https://www.npmjs.com/package/form-hoc) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save form-hoc
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'form-hoc'
+
+class Example extends Component {
+  render () {
+    return (
+      <MyComponent />
+    )
+  }
+}
+```
+
+## License
+
+MIT © [naisergic](https://github.com/naisergic)
---
+++ form-hoc/src/index.js
@@ -0,0 +1,94 @@
+import React, { PureComponent, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import {checkRegex} from './validation'
+
+export default class formHoc extends PureComponent {
+  static propTypes = {
+    isRedux: PropTypes.bool,
+    inputProps: PropTypes.object
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      errorMsg: '',
+      value: ''
+    }
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.handleOnBlur = this.handleOnBlur.bind(this)
+    this.checkValidations = this.checkValidations.bind(this)
+  }
+
+  checkValidations(value, checkValidation) {
+    const {inputProps} = this.props
+    let isError
+    const {validationsToCheck} = inputProps
+    if (Array.isArray(validationsToCheck) && validationsToCheck.length > 0 && checkValidation) {
+      validationsToCheck.some(item => {
+        const {regexVal, errorVal} = item
+        isError = checkRegex(regexVal, value)
+        if (isError) {
+          this.setState({
+            errorMsg: errorVal
+          })
+          return true
+        }
+        return false
+      })
+    }
+  }
+
+  handleOnBlur(e) {
+    const {inputProps} = this.props
+    const {checkValidationOnBlur, onBlur} = {inputProps}
+    this.checkValidations(this.state.value, checkValidationOnBlur)
+    if (typeof onBlur === 'function') {
+      this.props.onBlur(e)
+    }
+  }
+
+  handleOnChange(e) {
+    const {inputProps} = this.props
+    const {onChange, getValue, checkValidationOnError} = inputProps
+    this.setState({
+      value: e.target.value
+    })
+    this.checkValidations(e.target.value, checkValidationOnError)
+    /**
+    * you guyz don't need to check for the value
+    * call only if you need business logic on this function
+    * but for validation find a regex or develop a one
+    */
+    if (typeof onChange === 'function') {
+      this.props.onChange(e)
+    }
+    // please take value from this call back
+    if (typeof getValue === 'function') {
+      this.props.getValue(e.target.value)
+    }
+  }
+
+  render() {
+    const {isRedux, inputProps} = this.props
+    const {classNameForErrorSpan, classNameForErrorSpanParagraph} = inputProps
+    /**
+     * isRedux will check that is Application using Redux or not
+     */
+    if (isRedux) {
+      return null
+    } else {
+      return (
+        <Fragment>
+          <input
+            {...inputProps}
+            onChange={(e) => { this.handleOnChange(e) }}
+            onBlur={(e) => { this.handleOnBlur(e) }}
+          />
+          <span className={classNameForErrorSpan}>
+            <p className={classNameForErrorSpanParagraph}>{this.state.errorMsg}</p>
+          </span>
+        </Fragment>
+      )
+    }
+  }
+}
