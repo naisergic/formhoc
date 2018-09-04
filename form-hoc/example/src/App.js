f1e7981438e73867996a9317d44a678432975be2
naisergic <nsharma82@sapient.com>
2018-09-04 11:04:18 +0530

    fixing the example component

--- form-hoc/example/src/App.js
+++ form-hoc/example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'form-hoc'
+import formHoc from 'form-hoc'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <formHoc text='Modern React component module' />
       </div>
     )
   }
