09d53f630863be2f703b7dc4507545c32ffdcff8
naisergic <nsharma82@sapient.com>
2018-10-03 12:17:08 +0530

    css for select

--- example/src/App.js
+++ example/src/App.js
@@ -9,7 +9,7 @@ export default class App extends Component {
         <FormHOC inputProps={
           {
             type: 'input',
-            labelprops: {label: 'Email', inputprops:{className:"labels",id:"emailLabel"}},
+            labelprops: {label: 'Email:', inputprops:{className:"labels",id:"emailLabel"}},
             checkvalidationonblur: "true",
             validationstocheck: [{regexToCheck: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/, errorMsg: 'Invalid Email Format'}],
             errormsgprops:{
@@ -23,16 +23,23 @@ export default class App extends Component {
         <FormHOC inputProps={
           {
             type: 'password',
-            labelprops: {label: 'Password'},
+            labelprops: {label: 'Password:', inputprops:{className:"labels",id:"passwordLabel"}},
             checkvalidationonchange: "true",
-            validationstocheck: [{regexToCheck: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/, errorMsg: 'Invalid Password Format'}]
+            validationstocheck: [{regexToCheck: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/, errorMsg: 'Invalid Password Format'}],
+            errormsgprops:{
+              className:'errorClass',
+              id:'passwordError'
+            },
+            errormsgparagraphprops: {
+              id: 'passwordErrorPara'
+            }
           }} />
           <FormHOC inputProps={
           {
             type: 'select',
             options:[{value:"IN",label:"INDIA"},{value:"US",label:"USA"}],
-            labelprops: {label: 'Country'},
-            optionprops:{},
+            labelprops: {label: 'Country:', inputprops:{className:"labels",id:"countryLabel"}},
+            optionprops:{className:"options"},
             id:"select1",
             ariadescribedby:{
               ariadescribedbymsg:"list of country",
