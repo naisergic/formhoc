fafbb65763d255a28c4eaa42942c7df8dcdf233a
akri11 <akrishnatray@sapient.com>
2019-07-23 21:07:33 +0530

    Adding Example

--- example/src/ErrorComponent.jsx
+++ example/src/ErrorComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const ErrorComponet = (props)=>{
-  return <span>{props.children}</span>
+  return <span class="errorMsg">{props.children}</span>
 }
 
 export default ErrorComponet;
