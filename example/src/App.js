07abb200e66612d3f4b2ea16d0bc0dcb09a016f3
akri11 <akrishnatray@sapient.com>
2019-07-29 19:50:48 +0530

    Modifications

--- example/src/App.js
+++ example/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { FormInput, ReactForm } from 'reactFormWrapper'
+import { FormInput, ReactForm } from 'reactformwrapper'
 import './index.css'
 import {firstName,lastName,email,password, confirmPassword, submitButton,password1,confirmPassword1} from './FormJson';
 
